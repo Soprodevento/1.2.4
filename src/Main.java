0c7ccc44fa94dc7f264ca2175e9f6e207b53182e
Soprodevento <Sashinka2803@gmail.com>
2024-02-24 22:33:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int a = 123;
+    boolean b = false;
+    char c = 'x';
+    short d = 124;
+    double z= 101;
+    float x = 11.12f;
+    long v = 12;
+    byte q =1;
+        }
+    }
+
+
+
